a0e25fea56728db4dc717fbe8ead7fc3d9c723f6
Lawson Lewis <lawson@kurrawong.ai>
2024-02-13 10:16:48 +1000

    refinement of RDF
    refinement of the RDF conversion of the polygons extracted from Nola's hand drawn langauge map

--- datasets/fuzzymaps/nola/etl.py
+++ datasets/fuzzymaps/nola/etl.py
@@ -5,7 +5,7 @@ from shapely.geometry import shape
 from rdflib import Graph, URIRef, BNode, Literal, Namespace
 from rdflib.namespace import DCTERMS, GEO, PROV, RDF, RDFS, SDO, XSD
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     g = Graph()
     dataset_iri = URIRef("https://data.idnau.org/pid/nola")
     DS = Namespace(str(dataset_iri) + "/")
@@ -14,25 +14,25 @@ if __name__ == '__main__':
     g.bind("ego", EGO)
     g.bind("egor", EGOR)
     g.bind("geo", GEO)
-    features = gpd.read_file(Path(__file__).parent / 'features/features.shp')
+    features = gpd.read_file(Path(__file__).parent / "features/features.shp")
     grouped = features.groupby("featureId")
     for name, group in grouped:
         feature_iri = URIRef(str(DS) + name)
-        qual_geom = BNode()
         g.add((feature_iri, RDF.type, GEO.Feature))
-        g.add((qual_geom, RDF.type, EGO.QualifiedGeometry))
-        g.add((feature_iri, EGO.hasQualifiedGeometry, qual_geom))
 
         # Geometry
-        for geometry in group["geometry"]:
+        for index, row in group.iterrows():
+            qual_geom = BNode()
+            g.add((qual_geom, RDF.type, EGO.QualifiedGeometry))
+            g.add((feature_iri, EGO.hasQualifiedGeometry, qual_geom))
             geom = BNode()
             g.add((geom, RDF.type, GEO.Geometry))
-            wkt = shape(geometry).buffer(0)
+            wkt = shape(row["geometry"]).buffer(0)
             g.add((geom, GEO.asWKT, Literal(wkt, datatype=GEO.wktLiteral)))
             g.add((qual_geom, EGO.hasGeometry, geom))
             g.add((qual_geom, EGO.hasGeometryRole, EGOR.BoundaryEstimate))
-            g.add((qual_geom, EGO.hasEvidence, Literal("Hand drawn map")))
-            g.add((qual_geom, EGO.hasConfidence, Literal("low")))
+            g.add((qual_geom, EGO.hasEvidence, Literal(f"{row['confidence']} certainty boundary derived from hand drawn map")))
+            g.add((qual_geom, EGO.hasConfidence, Literal(row["confidence"])))
 
-
-        g.serialize(destination="features.ttl", format="longturtle")
+    parent_dir = Path(__file__).parent
+    g.serialize(destination=parent_dir / "features.ttl", format="longturtle")
